a2ddb8d2b200e8aee5c6b5af0a0e4f8f9bf2c2bd
Errol McFadden <errol.mcfadden@westpac.com.au>
2025-06-13 11:51:13 +1000

    Added some action methods

--- AspNetMvcCourse/Controllers/HomeController.cs
+++ AspNetMvcCourse/Controllers/HomeController.cs
@@ -6,8 +6,22 @@ using System.Web.Mvc;
 
 namespace AspNetMvcCourse.Controllers
 {
+    class Employee
+    {
+        public int EmployeeID { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+    }
+
     public class HomeController : Controller
     {
+        readonly Employee[] employees = new Employee[]
+        {
+            new Employee { EmployeeID = 1, Name = "Scott", Salary = 8000 },
+            new Employee { EmployeeID = 2, Name = "Smith", Salary = 2540 },
+            new Employee { EmployeeID = 3, Name = "Allen", Salary = 9400 },
+        };
+
         // GET: Home
         public ActionResult Index()
         {
@@ -22,6 +36,29 @@ namespace AspNetMvcCourse.Controllers
         public ActionResult Contact()
         {
             return View();
+        }
+
+        public ActionResult GetEmpName(int employeeID)
+        {
+            string matchedName = employees.FirstOrDefault(e => e.EmployeeID == employeeID)?.Name;
+
+            //return new ContentResult() { Content = matchedName, ContentType = "text/plain" };
+            return Content(matchedName, "text/plain");
+        }
+
+        public ActionResult GetPaySlip(int employeeID)
+        {
+            string fileName = "~/PaySlip" + employeeID + ".pdf";
+            return File(fileName, "application/pdf");
+        }
+
+        public ActionResult EmpFacebookPage(int employeeID)
+        {
+            if (!employees.Any(e => e.EmployeeID == employeeID))
+                return Content("Invalid employee id");
+
+            string url = "http://www.facebook.com/emp" + employeeID;
+            return Redirect(url);
         }
     }
 }

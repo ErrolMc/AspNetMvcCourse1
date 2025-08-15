24698a5d52eff185a32e45cc0fd9d7008b520089
Errol McFadden <errolmcfadden@hotmail.com>
2025-08-15 13:39:48 +1000

    RedirectToAction method

---
+++ AspNetMvcCourse/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspNetMvcCourse.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult Login(string userName, string password)
+        {
+            if (userName == "admin" && password == "manager")
+                return RedirectToAction("Dashboard", "Admin");
+
+            return RedirectToAction("InvalidLogin");
+        }
+
+        public ActionResult InvalidLogin()
+        {
+            return View();
+        }
+    }
+}
